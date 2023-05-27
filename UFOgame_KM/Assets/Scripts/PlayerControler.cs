6bc853b82597a905eccde1d963ef2af15446d137
karminbi3 <karolina1902km@gmail.com>
2023-05-27 16:27:35 +0200

    Added Camera & Pickup controllers

--- UFOgame_KM/Assets/Scripts/PlayerControler.cs
+++ UFOgame_KM/Assets/Scripts/PlayerControler.cs
@@ -6,24 +6,24 @@ public class PlayerControler : MonoBehaviour
 {
     Rigidbody2D rb2d;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();   
     }
-
-    // Update is called once per frame
-    // Tick 
 
     private void FixedUpdate()
     {
         float moveVertical = Input.GetAxis("Vertical");
         float moveHorizontal = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-        rb2d.AddForce(movement * 10);
+        rb2d.AddForce(movement * 20);
     }
-    void Update()
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("PickUp"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
