e602a44c6a14598d4ee95c123db0f5377c29e8c2
karminbi3 <karolina1902km@gmail.com>
2023-05-29 18:49:51 +0200

    added text

--- UFOgame_KM/Assets/Scripts/PlayerControler.cs
+++ UFOgame_KM/Assets/Scripts/PlayerControler.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControler : MonoBehaviour
 {
+    public Text scoreText;
+    public Text winText;
     Rigidbody2D rb2d;
     private int count = 0;
 
@@ -17,15 +20,24 @@ public class PlayerControler : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         float moveHorizontal = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-        rb2d.AddForce(movement * 20);
+        rb2d.AddForce(movement * 8);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("PickUp"))
         {
-            count++;
+            count++; //zwiêksz wartoœæ o 1
             Destroy(collision.gameObject);
+            UpadateScoreText();
         }
     }
+
+    void UpadateScoreText() 
+    {
+        scoreText.text = "Wynik: " + count;
+    }
+
+
 }
+
